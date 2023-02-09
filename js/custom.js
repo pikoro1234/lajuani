5169da7334acb73ba4d15885dbe5f4c9b7400c76
pikoro1234 <48534970+pikoro1234@users.noreply.github.com>
2023-02-09 23:25:49 +0100

    accion de cambio color de fonodo blanco y negro

--- js/custom.js
+++ js/custom.js
@@ -5,20 +5,22 @@ $(document).ready(function () {
         $('.logo-center-la-juani').fadeOut(1300);    
 
     }, 1500);
+
+    // cambiamos el color de fondo y las letras
+    $(document).on('click', '.btn-cambio', function () {        
+        if ($(this).attr('attr-color') == 'negro') {        
+            $('.fondo-change').css('background-color', 'black')
+            
+        } else {
+            $('.fondo-change').css('background-color', 'white')
+        }
+    })
     
     // scroll fullpage
     $('#fullpage').fullpage({        
         navigation: false,
         navigationPosition: 'bottom',
         scrollOverflow: true,                
-        css3:true
-        // fadingEffect: true,
-        // fadingEffect: 'slides',
-        // fadingEffect: 'sections',
-        // continuousHorizontal: true,
-        // autoScrolling: true,
-        // scrollHorizontally: true,        
-    });
-    
-    window.fullpage_api.setScrollingSpeed(2000);
+        css3:true       
+    });        
 });
