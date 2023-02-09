d807afd6f314e632b68fe788fec5d3b4147c7583
pikoro1234 <48534970+pikoro1234@users.noreply.github.com>
2023-02-09 23:09:04 +0100

    desktop single proyecto

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,24 @@
-$(document).ready(function () {        
-    $('#fullpage').fullpage({
+$(document).ready(function () {
+
+    //escondemos el logo central despues de unos segundo
+    setTimeout(() => {   
+        $('.logo-center-la-juani').fadeOut(1300);    
+
+    }, 1500);
+    
+    // scroll fullpage
+    $('#fullpage').fullpage({        
         navigation: false,
         navigationPosition: 'bottom',
-        scrollOverflow: true,        
-        menu: '#myMenu',
+        scrollOverflow: true,                
+        css3:true
         // fadingEffect: true,
         // fadingEffect: 'slides',
         // fadingEffect: 'sections',
         // continuousHorizontal: true,
-        autoScrolling: true,
-        scrollHorizontally: true,
+        // autoScrolling: true,
+        // scrollHorizontally: true,        
     });
+    
+    window.fullpage_api.setScrollingSpeed(2000);
 });
