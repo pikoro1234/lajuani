d50f05d844b9e7f752022237cf805a64245db3e4
pikoro1234 <donjuan.jfc@gmail.com>
2023-02-21 23:15:03 +0100

    estilos desktop completos

--- js/custom.js
+++ js/custom.js
@@ -7,12 +7,16 @@ $(document).ready(function () {
     }, 1500);
 
     // cambiamos el color de fondo y las letras
-    $(document).on('click', '.btn-cambio', function () {        
+    $(document).on('click', '.btn-cambio', function () {  
+        $('.btn-cambio').find('.texto-change').removeClass('active')
+        $(this).find('.texto-change').addClass('active')
         if ($(this).attr('attr-color') == 'negro') {        
             $('.fondo-change').css('background-color', 'black')
+            $('.texto-change').addClass('change-color-text')            
             
         } else {
             $('.fondo-change').css('background-color', 'white')
+            $('.texto-change').removeClass('change-color-text')
         }
     })
     
